0dc21923b0085de6d26f5fc0e51afc96bb6f2297
roydaw <5164541@gmail.com>
2017-09-28 13:12:58 -0600

    nothing

--- android/app/src/main/java/com/thefunteam/android/RegistrationPresenter.java
+++ android/app/src/main/java/com/thefunteam/android/RegistrationPresenter.java
@@ -1,7 +1,7 @@
 package com.thefunteam.android;
 
 public class RegistrationPresenter {
-
+//lol
     void register(String username, String password) {
         NextLayerFacade.getInstance().register(username, password);
     }
