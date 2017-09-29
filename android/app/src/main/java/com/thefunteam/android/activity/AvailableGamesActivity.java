c73fe8ba2b08c2877816f217208d3c885c962d1c
roydaw <5164541@gmail.com>
2017-09-29 10:44:43 -0600

    Added call to server.

--- android/app/src/main/java/com/thefunteam/android/activity/AvailableGamesActivity.java
+++ android/app/src/main/java/com/thefunteam/android/activity/AvailableGamesActivity.java
@@ -5,8 +5,17 @@ import android.os.Bundle;
 import com.thefunteam.android.R;
 import com.thefunteam.android.model.Atom;
 import com.thefunteam.android.model.Model;
+import com.thefunteam.android.presenter.AvailableGamesPresenter;
 
-public class AvailableGamesActivity extends AppCompatActivity {
+public class AvailableGamesActivity extends ObservingActivity {
+
+    AvailableGamesPresenter availableGamesPresenter = new AvailableGamesPresenter();
+
+    AvailableGamesActivity() {
+        super();
+
+        presenter = availableGamesPresenter;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ android/app/src/main/java/com/thefunteam/android/presenter/AvailableGamesPresenter.java
@@ -0,0 +1,27 @@
+package com.thefunteam.android.presenter;
+
+import com.thefunteam.android.activity.AvailableGamesActivity;
+import com.thefunteam.android.model.Atom;
+import com.thefunteam.android.model.Model;
+
+import java.util.Observable;
+
+public class AvailableGamesPresenter extends Presenter {
+
+    AvailableGamesActivity availableGamesActivity;
+
+    AvailableGamesPresenter(AvailableGamesActivity availableGamesActivity) {
+        super();
+
+        this.availableGamesActivity = availableGamesActivity;
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        Model model = Atom.getInstance().getModel();
+
+        if (model.getCurrentGame() != null) {
+            // start the current game activity here.
+        }
+    }
+}
