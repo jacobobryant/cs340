c9645a99d49fd904961bcd5601769b63c7f9dc79
techimera <masteraile@gmail.com>
2017-10-14 15:53:12 -0600

    InGameModle Renewaled

---
+++ android/app/src/main/java/com/thefunteam/android/model/InGameModel/TrainCard.java
@@ -0,0 +1,15 @@
+package com.thefunteam.android.model.InGameModel;
+
+public class Train {
+    //color of the train
+    private String color;
+
+    public Train(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
