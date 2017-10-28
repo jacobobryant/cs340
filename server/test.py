b0c732eb4582fd0d105f09d8fcb3ec64efb3a2ea
Jacob O'Bryant <jacobobryant7@gmail.com>
2017-10-28 00:48:50 -0600

    Cleanup, add to test cases

--- server/test.py
+++ server/test.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+import unittest
 import requests
 import json
+from copy import deepcopy
 from random import randint
+
+def assoc(m, *args):
+    assert len(args) % 2 == 0
+    ret = deepcopy(m)
+    for i in range(0, len(args), 2):
+        ret[args[i]] = args[i+1]
+    return ret
 
 def select(d, *keys):
     return {k: d[k] for k in keys}
 
 def hit(endpoint, data):
     response = json.loads(requests.post('http://localhost:8080' + endpoint, data=json.dumps(data)).text)
-    try:
-        response["currentGame"]["openRoutes"] = "[removed by test.py]"
-    except (KeyError, TypeError) as e:
-        pass
     print(endpoint)
     print_json(data)
     print_json(response)
@@ -20,7 +25,12 @@ def hit(endpoint, data):
     return response
 
 def print_json(data):
-    print(json.dumps(data, indent=2))
+    _data = deepcopy(data)
+    try:
+        _data["currentGame"]["openRoutes"] = "[removed by test.py]"
+    except (KeyError, TypeError) as e:
+        pass
+    print(json.dumps(_data, indent=2))
 
 def gen_user():
     n = 0
@@ -29,51 +39,79 @@ def gen_user():
         n += 1
 users = gen_user()
 
-hit('/clear', None)
-# test register
-user1 = users.__next__()
-model1 = hit('/register', user1)
+class TestServer(unittest.TestCase):
 
-# test login
-hit('/login', user1)
+    def run(self, result=None):
+        if not (result.failures or result.errors):
+            super().run(result)
 
-# test create
-hit('/create', select(model1, 'sessionId'))
+    @staticmethod
+    def new_user():
+        return select(hit('/register', users.__next__()), 'sessionId')
 
-# test join
-user2 = users.__next__()
-model2 = hit('/register', user2)
-hit('/join', {'sessionId': model2['sessionId'],
-              'gameId': model2['availableGames'][0]['gameId']})
+    @staticmethod
+    def join_first(user):
+        request = deepcopy(user)
+        request['gameId'] = hit('/state', user)['availableGames'][0]['gameId']
+        hit('/join', request)
 
-# test leave
-model2 = hit('/leave', select(model2, 'sessionId'))
+    def raises(self, fn, *args, **kwargs):
+        self.assertRaises(AssertionError, fn, *args, **kwargs)
 
-# test start
-hit('/join', {'sessionId': model2['sessionId'],
-              'gameId': model2['availableGames'][0]['gameId']})
-hit('/start', select(model2, 'sessionId'))
+    def setUp(self):
+        hit('/clear', None)
+        self.user1 = self.new_user()
+        self.user2 = self.new_user()
+        self.user3 = self.new_user()
+        hit('/create', self.user1)
 
-# test state
-client_model = hit('/state', select(model1, 'sessionId'))
+    def test_register(self):
+        u = users.__next__()
+        hit('/register', u)
+        self.raises(lambda: hit('/register', u))
 
-cards = client_model["currentGame"]["players"][0]["destCards"]
-model1['dest'] = cards[0]
-hit('/return-dest', select(model1, 'sessionId', 'dest'))
+    def test_login(self):
+        u = users.__next__()
+        hit('/register', u)
+        hit('/login', u)
+        hit('/login', u)
 
-model1['dest'] = cards[1]
-try:
-    hit('/return-dest', select(model1, 'sessionId', 'dest'))
-    assert False
-except AssertionError:
-    pass
+    def test_join_leave(self):
+        self.join_first(self.user2)
+        hit('/leave', self.user2)
 
-#model2 = hit('/state', select(model2, 'sessionId'))
-try:
-    hit('/return-dest', select(model2, 'sessionId'))
-    assert False
-except AssertionError:
-    pass
+    def test_start(self):
+        self.raises(lambda: hit('/start', self.user1))
+        self.join_first(self.user2)
+        hit('/start', self.user1)
 
-model2['dest'] = None
-hit('/return-dest', select(model2, 'sessionId', 'dest'))
+    def test_return_dest(self):
+        self.join_first(self.user2)
+        hit('/start', self.user1)
+
+        cards = hit('/state', self.user1)["currentGame"]["players"][0]["destCards"]
+        request = deepcopy(self.user1)
+        request['dest'] = cards[0]
+
+        hit('/return-dest', self.user1)
+
+        request['dest']['points'] += 1
+        self.raises(lambda: hit('/return-dest', request))
+
+        request['dest']['points'] -= 1
+        hit('/return-dest', request)
+
+        request['dest'] = cards[1]
+        self.raises(lambda: hit('/return-dest', request))
+
+        hit('/return-dest', self.user2)
+
+    def test_chat(self):
+        message = "hello there"
+        state = hit('/chat', assoc(self.user1, "message", message))
+        assert message in state['currentGame']['messages'][0]
+        self.raises(lambda: hit('/chat', self.user1))
+        self.raises(lambda: hit('/chat', assoc(self.user2, "message", "foo")))
+
+if __name__ == "__main__":
+    unittest.main()
