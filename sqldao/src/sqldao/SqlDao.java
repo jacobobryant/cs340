511d1591453068944c53d24b8ee7da5c688e719b
Jacob O'Bryant <jacobobryant7@gmail.com>
2017-11-16 17:30:28 -0700

    Start working on sqldao

--- sqldao/src/sqldao/SqlDao.java
+++ sqldao/src/sqldao/SqlDao.java
@@ -3,12 +3,50 @@ package sqldao;
 import shared.dao.Dao;
 import shared.dao.Event;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SqlDao implements Dao {
+    public static final String DB = "jdbc:sqlite:ticket.db";
+
+    interface Transaction {
+        void run(Statement st) throws SQLException;
+    }
+
     public void init(boolean wipe) {
         System.out.println("init");
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        runTransaction((Statement st) -> {
+            if (wipe) {
+                st.executeUpdate("drop table if exists events");
+                st.executeUpdate("drop table if exists states");
+            }
+            st.executeUpdate("create table if not exists events " +
+                    "(id integer, endpoint string, json string)");
+            st.executeUpdate("create table if not exists states " +
+                    "(id integer, state blob)");
+        });
+        System.out.println("finished SqlDao.init()");
+    }
+
+    private void runTransaction(Transaction transaction) {
+        try {
+            Connection conn = DriverManager.getConnection(DB);
+            Statement st = conn.createStatement();
+            st.setQueryTimeout(30);
+            transaction.run(st);
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void startTransaction() {
@@ -35,5 +73,9 @@ public class SqlDao implements Dao {
     public List<Event> getEventsAfter(int eventId) {
         System.out.println("getting recent events");
         return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        new SqlDao().init(false);
     }
 }
